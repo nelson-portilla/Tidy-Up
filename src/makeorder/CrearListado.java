6f380ed449a15da68472d4d02b761f55c29e8f88
nelson-portilla <nelson.herrera@correounivalle.edu.co>
2015-05-01 01:14:41 -0500

    Primera Version: Copiar archivo, Leer etiquetas

---
+++ src/makeorder/CrearListado.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makeorder;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CrearListado {
+
+    public CrearListado() {
+        String[] cmd = {"cmd.exe", "/C", "cd", "C:/Users/USUARIO/Music", "&", "dir", "/b", ">", "nombres.txt"};
+
+        Process pr;
+        try {
+            pr = Runtime.getRuntime().exec(cmd);
+            System.out.println(cmd);
+            InputStream inputstream = pr.getInputStream();
+            InputStreamReader isr = new InputStreamReader(inputstream);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                System.out.println("--->LISTADO CREADO EXITOSAMENTE");
+            }
+        } catch (Exception ex) {
+            System.out.println("Ha ocurrido un error al crear el listado. Error: " + ex);
+        }
+    }
+
+}
---
+++ src/makeorder/ObtenerNombres.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makeorder;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ObtenerNombres {
+    
+    public String [] leerArchivo(){
+        String [] canciones={};
+        return canciones;
+    }
+    
+}
